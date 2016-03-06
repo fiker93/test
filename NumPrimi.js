ac77a4435f1e33636519361c1e7e1e1bfbac0d33
Riccardo Focanti <you@example.com>
2016-03-06 21:43:24 +0100

    intervallo

--- NumPrimi.js
+++ NumPrimi.js
@@ -1,6 +1,9 @@
 var i;
 var isNumeroPrimo = function(n){
   var i;
+  if(n <= 0){
+    return false;
+  }
   for(i = 2; i < n; i++){
    if(n % i === 0){
     return false;
@@ -8,11 +11,31 @@ var isNumeroPrimo = function(n){
   }
   return true;
 };
-var cazzo = false;
+var numeriPrimiIntervallo = function (inizio,fine){
+  var toReturn = [];
+  for(i=inizio; i <= fine; i++){
+    if(isNumeroPrimo(i)){
+      toReturn[toReturn.length]=i;
+    }
+  }
+  return toReturn;
+}
+console.log(numeriPrimiIntervallo(1000,5000));
+/*var cazzo = false;
 for (i = 0; !cazzo; i++){
   if (i <= 100 && isNumeroPrimo(i)) {
     console.log(i);}
     else {
-      cazzo = true;
+      cazzo a= true;
     }
 }
+var trovati = 0;
+var i=0;
+do {
+  if(isNumeroPrimo(i)){
+    console.log(i);
+    trovati++;
+  }
+  i++;
+}while(trovati <= 100 )
+*/
