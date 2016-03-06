66cfdf77078aa74f46b3e7c37f2c21dace050053
Riccardo Focanti <you@example.com>
2016-03-06 22:46:21 +0100

    aggiunta todo list

---
+++ ToDoList.js
@@ -0,0 +1,44 @@
+/*var listaToDo = function(){
+  var toReturn = [];
+  var aggiungiToDo = function(testo){
+    var toDo = {
+      testo: testo,
+      fatto: false
+    };
+    toReturn.push(toDo);
+  };
+  aggiungiToDo("ciao");
+  return toReturn;
+};
+
+console.log(listaToDo());
+*/
+var nuovoModuloToDo = function(){
+  var toDos = [];
+  var toReturn = {
+    listaToDo: function(){
+      return toDos;
+    },
+    aggiungiToDo: function(testo){
+      var toDo = {
+        testo: testo,
+        fatto: false
+      };
+      toDos.push(toDo);
+    },
+    mettiSpunta: function(i){
+      toDos[i].fatto = true;
+    },
+    eliminaToDo: function(i){
+      toDos.splice(i,1);
+    }
+  };
+  return toReturn;
+};
+var primaListaTodo = nuovoModuloToDo();
+primaListaTodo.aggiungiToDo("cipolle");
+primaListaTodo.aggiungiToDo("olio");
+primaListaTodo.aggiungiToDo("sale");
+primaListaTodo.mettiSpunta(1);
+primaListaTodo.eliminaToDo(2);
+console.log(primaListaTodo.listaToDo());
